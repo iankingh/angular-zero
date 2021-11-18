6a8e5c6babe2a7e04b58ef6d60cee82cfcb38e6f
ian <ian>
2021-11-18 10:25:12 +0800

    新增:datepick-min-max : 新增日期元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DatepickerMinMaxExampleComponent } from './datepicker-min-max-example/datepicker-min-max-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
+    DatepickerMinMaxExampleComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/datepicker-min-max-example/datepicker-min-max-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datepicker-min-max-example',
+  templateUrl: './datepicker-min-max-example.component.html',
+  styleUrls: ['./datepicker-min-max-example.component.css']
+})
+export class DatepickerMinMaxExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
